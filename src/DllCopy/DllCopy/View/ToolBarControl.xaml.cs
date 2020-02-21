f040eb5387de115579d551f97f66e56d8a89bb5b
Håvard Moås <hmo@dips.no>
2020-02-22 00:12:50 +0100

    Added missing buttons

--- src/DllCopy/DllCopy/View/ToolBarControl.xaml.cs
+++ src/DllCopy/DllCopy/View/ToolBarControl.xaml.cs
@@ -25,6 +25,38 @@ namespace DllCopy.View
             InitializeComponent();
         }
 
+        public static readonly DependencyProperty BackgroundProperty = DependencyProperty.Register("Background", typeof(Brush), typeof(ToolBarControl), new PropertyMetadata(Brushes.LightGray));
+
+        public new Brush Background
+        {
+            get { return (Brush)GetValue(BackgroundProperty); }
+            set { SetValue(BackgroundProperty, value); }
+        }
+
+        public static readonly DependencyProperty TextColorProperty = DependencyProperty.Register("TextColor", typeof(Brush), typeof(ToolBarControl), new PropertyMetadata(Brushes.White));
+
+        public Brush TextColor
+        {
+            get { return (Brush)GetValue(TextColorProperty); }
+            set { SetValue(TextColorProperty, value); }
+        }
+
+        public static readonly DependencyProperty ButtonColorProperty = DependencyProperty.Register("ButtonColor", typeof(Brush), typeof(ToolBarControl), new PropertyMetadata(default(Brush)));
+
+        public Brush ButtonColor
+        {
+            get { return (Brush)GetValue(ButtonColorProperty); }
+            set { SetValue(ButtonColorProperty, value); }
+        }
+
+        public static readonly DependencyProperty ButtonMouseOverColorProperty = DependencyProperty.Register("ButtonMouseOverColor", typeof(Brush), typeof(ToolBarControl), new PropertyMetadata(default(Brush)));
+
+        public Brush ButtonMouseOverColor
+        {
+            get { return (Brush)GetValue(ButtonMouseOverColorProperty); }
+            set { SetValue(ButtonMouseOverColorProperty, value); }
+        }
+
         void PnMouseMove(object sender, System.Windows.Input.MouseEventArgs e)
         {
             if (e.LeftButton == MouseButtonState.Pressed)
@@ -57,6 +89,29 @@ namespace DllCopy.View
                 }
                 
             }
+        }
+
+        private void ToggleWindowSize(object sender, RoutedEventArgs e)
+        {
+            switch (App.Current.MainWindow.WindowState)
+            {
+                case WindowState.Normal:
+                    App.Current.MainWindow.WindowState = WindowState.Maximized;
+                    break;
+                case WindowState.Maximized:
+                    App.Current.MainWindow.WindowState = WindowState.Normal;
+                    break;
+            }
+        }
+
+        private void MinimizeWindow(object sender, RoutedEventArgs e)
+        {
+            App.Current.MainWindow.WindowState = WindowState.Minimized;
+        }
+
+        private void CloseApp(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
         }
     }
 }
